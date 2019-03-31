f6d8e2264fcb1db64f036cc9a388282db517658a
Andying2019 <49111403+Andying2019@users.noreply.github.com>
2019-03-31 12:05:40 +0800

    Update group.sql

--- group.sql
+++ group.sql
@@ -1,4 +1,6 @@
+-- 查询有多少人选了3-105这门课
+select count(*) from score where cno='3-105';
 select cno,count(*) from score GROUP BY cno;
 select cno,count(*) from score GROUP BY cno having count(*)>=5;
 select cno,count(*) from score;
-select count(*) from score where cno='3-105';
+
