9d75fc824855168366cba84bd760968dd686ac04
Andying2019 <49111403+Andying2019@users.noreply.github.com>
2019-03-31 12:37:25 +0800

    Update and rename group.sql to group by

--- group.sql
+++
@@ -1,6 +0,0 @@
--- 查询有多少人选了3-105这门课
-select count(*) from score where cno='3-105';
-select cno,count(*) from score GROUP BY cno;
-select cno,count(*) from score GROUP BY cno having count(*)>=5;
-select cno,count(*) from score;
-
